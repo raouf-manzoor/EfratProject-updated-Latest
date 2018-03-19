cf72da33ee171253f3aeb8c689c2a428ac985e4a
Raouf <RaoufManzoor@yahoo.com>
2018-03-20 00:04:44 +0500

    Added Efrat Repository
    Added Efrat Repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
-
+import { SignUpComponentComponent } from './signUp/sign-up-component.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { BookAcarComponent } from './bookAcar/book-acar.component';
+import { HomeComponent } from './home/home.component';
+const appRoutes:Routes =[
+// {path:'home',component:AppComponent},
+{path:'signUp',component:SignUpComponentComponent},
+{path:'home/signUp',component:SignUpComponentComponent},
+{path:'home/signIn',component:SignUpComponentComponent},
+{path:'signIn',component:SignInComponent},
+{path:'bookAcar',component:BookAcarComponent},
+{path:'home',component:HomeComponent},
+{path:'',redirectTo:'/home',pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponentComponent,
+    SignInComponent,
+    BookAcarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bookAcar/book-acar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-acar',
+  templateUrl: './book-acar.component.html',
+  styleUrls: ['./book-acar.component.css']
+})
+export class BookAcarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
